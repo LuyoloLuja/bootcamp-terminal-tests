d306d5c6762af97bc514192ecbc835dfeb73983d
Luyolo <luyolom19@gmail.com>
2020-08-06 18:29:58 +0200

    added findItemsOver20 tests

---
+++ findItemsOver20.js
@@ -0,0 +1,12 @@
+module.exports = function findItemsOver20(list){
+    var increment = [];
+    for(var i = 0; i < list.length; i++){
+        var eachItem = list[i];
+        if(list[i].qty > 20){
+          increment.push(eachItem);
+        }
+    }
+    return increment;
+}    
+    
+    
