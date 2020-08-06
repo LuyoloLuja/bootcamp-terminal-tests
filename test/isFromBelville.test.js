3dbeef13b3bcdd2cd7846f1312ccb7b898c99843
Luyolo <luyolom19@gmail.com>
2020-08-06 15:11:53 +0200

    added isFromBelville test

---
+++ test/isFromBelville.test.js
@@ -0,0 +1,11 @@
+let assert = require('assert');
+let isFromBelville = require('../isFromBelville.js');
+
+describe('The isFromBelville function', function(){
+    it('Should return true for CY', function(){
+        assert.equal(true, isFromBelville("CY"));
+    });
+    it('Should return false for CJ', function(){
+        assert.equal(false, isFromBelville("CJ"))
+    })
+});
