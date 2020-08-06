c07470122170e822808d1b91b0637c2387786ebd
Luyolo <luyolom19@gmail.com>
2020-08-06 16:55:29 +0200

    added transportFee tests

---
+++ test/transportFee.test.js
@@ -0,0 +1,17 @@
+let assert = require('assert');
+let transportFee = require('../transportFee.js');
+
+describe('The transportFee function', function(){
+    it('Should check transportFee for morning shift', function(){
+        var morning = transportFee("R20");
+        assert.equal(morning, transportFee("R20"))
+    })
+    it('Should check transportFee for afternoon aftenoon', function(){
+        var afternoon = transportFee("R10");
+        assert.equal(afternoon, transportFee("R10"));
+    })
+    it('Should check  transportFee for nightshift', function(){
+        var nightshift = transportFee("free");
+        assert.equal(nightshift, transportFee("free"));
+    })
+})
