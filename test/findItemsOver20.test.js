7015f640fde70150e326123d8d6b1bd9611c1a82
Luyolo <luyolom19@gmail.com>
2020-08-07 12:44:40 +0200

    fixed itemsOver20 test

--- test/findItemsOver20.test.js
+++ test/findItemsOver20.test.js
@@ -1,25 +1,3 @@
-/*let assert = require('assert');
-let findItemsOver20 = require('../findItemsOver20');
-
-describe('The findItemsOver20 function', function(){
-    var itemList = [
-        {name : 'apples', qty : 10},
-        {name : 'pears', qty : 37},
-        {name : 'bananas', qty : 27},
-        {name : 'apples', qty : 3},
-    ];
-
-    it('Should return true for pears', function(){
-        assert.equal(itemList.pears, undefined);
-    })
-
-    it('Should return true for bananas', function(){
-        assert.equal(itemList.bananas, undefined);
-    })
-    it('Should return false for apples', function(){
-        assert.equal(itemList.apples, undefined);
-    })
-})*/
 let assert = require("assert");
 let findItemsOver20 = require("../findItemsOver20");
 
