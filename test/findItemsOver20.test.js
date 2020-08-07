8069d88f772b6b5a8d8dc358b73a2125bc0c3dfd
Luyolo <luyolom19@gmail.com>
2020-08-07 12:42:55 +0200

    fixed itemsOver20 test

--- test/findItemsOver20.test.js
+++ test/findItemsOver20.test.js
@@ -24,10 +24,13 @@ let assert = require("assert");
 let findItemsOver20 = require("../findItemsOver20");
 
 describe('findItemsOver20 function' , function(){
-    it('should  return all the products that have a quantity higher than 20.  ' , function(){
-        assert.deepEqual(findItemsOver20([{name:"pears", qty:24}]), [{name:"pears", qty:24}])
+    it('Should return pears since their quantity is higher than 20.', function(){
+        assert.deepEqual(findItemsOver20([{name:"pears", qty:37}]), [{name:"pears", qty:37}])
     });
-    it('should  return an empty Array if the is no quantity product higher than 20. ' , function(){
-        assert.deepEqual(findItemsOver20([{name:"bananas", qty:17},{name:"pears", qty:17}]), [])
+    it('Should return bananas since their quantity is higher than 20.', function(){
+        assert.deepEqual(findItemsOver20([{name : 'bananas', qty:27}]), [{name : 'bananas', qty:27}])
+    })
+    it('Should return empty array for apples' , function(){
+        assert.deepEqual(findItemsOver20([{name : 'apples', qty : 10},{name : 'apples', qty : 10}]), [])
     });
 });
