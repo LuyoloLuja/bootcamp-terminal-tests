830a8efdc9911b6ba4352191ca6e93b21c8ea210
Luyolo <luyolom19@gmail.com>
2020-08-07 13:01:03 +0200

    added itemsOver test

--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -1,24 +1,3 @@
-/*let assert = require('assert');
-let findItemsOver = require('../findItemsOver')
-
-describe('The findItemsOver function', function(){
-    var result = [
-        {name:'apples', qty:40},
-        {name:'pears', qty:20},
-        {name:'bananas', qty:23},
-        {name:'apples', qty:37},
-    ];
-
-    it('Should return true for apples', function(){
-        assert.equal(result.apples, undefined);
-    })
-    it('Should return false for pears', function(){
-        assert.equal(result.pears, undefined);
-    })
-    it('Should return false for bananas', function(){
-        assert.equal(result.bananas, undefined);
-    })
-})*/
 let assert = require("assert");
 let findItemsOver = require("../findItemsOver");
 
